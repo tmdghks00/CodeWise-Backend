6f34a469eef6889113666634871f90b50674372e
SeungHwan Oh <wxtq22@naver.com>
2025-07-10 18:15:57 +0900

    .

--- backend/src/test/java/com/codewise/ApplicationTests.java
+++ backend/src/test/java/com/codewise/ApplicationTests.java
@@ -1,10 +1,10 @@
-package com.codewise.backend;
+package com.codewise;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BackendApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
