b9c19affd17d0850f3eb79323c27815c6cbbdd64
SeungHwan Oh <wxtq22@naver.com>
2025-07-12 15:23:16 +0900

    feat: 사용자 리포지토리 정의

--- backend/src/main/java/com/codewise/repository/UserRepository.java
+++ backend/src/main/java/com/codewise/repository/UserRepository.java
@@ -2,7 +2,6 @@ package com.codewise.repository;
 
 import com.codewise.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
 
 // 사용자(User) 정보를 DB 에서 조회하고 관리하기 위한 JPA 리포지토리 인터페이스
